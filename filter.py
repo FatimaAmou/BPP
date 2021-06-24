d39728306a2b813a1dd99f0c8ca9004f965fdcb7
FatimaAmou <86428086+FatimaAmou@users.noreply.github.com>
2021-06-24 21:57:44 +0200

    Update filter.py

--- filter.py
+++ filter.py
@@ -5,7 +5,7 @@ def es_primo(n):
             primo = False
     return primo
 
-numeros = [3, 4, 8, 5, 5, 22, 13]
+numeros = [3, 4, 8, 5, 5, 22, 13,10,20]
 
 
 primo= list(filter(es_primo, numeros))
